d382ae7db7369bd2c513072e8ead67ae1e11503d
jacqie williamson <jacqieq@me.com>
2021-09-14 21:02:59 +0100

    add middleware

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(PORT, () => {
   console.log("server listening on port:" + PORT);
 });
