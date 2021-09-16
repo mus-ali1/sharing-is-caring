701adb0462fdce0cc59697c5052dacd0deaa26ee
Paul Aston <odders17@gmail.com>
2021-09-16 22:04:15 +0100

    create fields  and columns for user database

--- model/User.js
+++ model/User.js
@@ -11,3 +11,51 @@ class User extends Model {
       return bcrypt.compareSync(loginPw, this.password);
     }
   }
+
+// create fields/columns for User model
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8]
+        }
+      },
+      location: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+      profile_picture: {
+        type: DataTypes.BLOB('long'),
+        allowNull: true,
+        defaultValue: null,
+      }, 
+      bio: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
+    },  
