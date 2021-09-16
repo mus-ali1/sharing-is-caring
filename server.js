d63009e70caa195bca6979f1d5e47def53608f5a
Mustafa Ali <mustafamire@gmail.com>
2021-09-16 18:54:21 +0100

    Add: declared handlebars & app.engine + app.set

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const path = require("path");
+const handlebars = require("express-handlebars");
 const app = express();
 const sequelizeStore = require("connect-session-sequelize");
 const session = require("express-session");
+const hbs = handlebars.create();
 
 const dbConnection = require("./config/connection");
 
 const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
