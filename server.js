547b26059ddf6293a3f8803b0348eec9adc31354
jacqie williamson <jacqieq@me.com>
2021-09-16 18:33:40 +0100

    Added sequelize, db connection, express session

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const sequelizeStore = require("connect-session-sequelize");
+const session = require("express-session");
 
+const dbConnection = require("./config/connection");
+
+const routes = require("./controllers");
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log("server listening on port:" + PORT);
+dbConnection.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("server listening on port:" + PORT);
+  });
 });
