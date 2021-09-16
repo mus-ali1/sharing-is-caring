8acc26c08cce9a2286c0b569709bcff80673f516
Paul Aston <odders17@gmail.com>
2021-09-16 21:50:57 +0100

    created user model

--- model/User.js
+++ model/User.js
@@ -3,3 +3,11 @@ const bcrypt = require('bcrypt'); // hash and save passwords on node
 
 // Set up a config file
 const sequelize = require('../config/connection.js');
+
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
