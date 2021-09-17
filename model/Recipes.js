86da6d78dc54ef63ac888033564764ece210eb9f
jacqie williamson <jacqieq@me.com>
2021-09-17 19:41:12 +0100

    Declared data types & model in recipes db

---
+++ model/Recipes.js
@@ -0,0 +1,14 @@
+const { Model, DataTypes } = require("sequelize");
+// Set up a config file
+const sequelize = require("../config/connection.js");
+
+// //creating columns for recipes db
+// class Recipes extends Model {}
+// Recipes.init({
+//   id: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+// });
