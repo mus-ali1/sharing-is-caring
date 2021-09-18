c9b6628ccfd6a3b0a9e0c3371972d7854bc22328
jacqie williamson <jacqieq@me.com>
2021-09-18 14:04:15 +0100

    Added comments clmn to comment db

--- model/Comment.js
+++ model/Comment.js
@@ -1,55 +1,58 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Comment extends Model { }
+class Comment extends Model {}
 
 Comment.init(
-    {
-
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-
-        user_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "user",
-                key: "id",
-            },
-        },
-
-        recipe_id: {
-            type: DataTypes.INTEGER,
-            references: {
-
-                model: "recipe",
-                key: "id",
-            },
-        },
-
-        upvote: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: null,
-        },
-
-        downvote: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-            defaultValue: null,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
-    {
-        sequelize,
-        tableName: "comment",
-        freezeTableName: true,
-        underscored: true,
-        timestamps: false,
 
-    }
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+
+    recipe_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "recipe",
+        key: "id",
+      },
+    },
+
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      defaultValue: null,
+    },
+
+    upvote: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+
+    downvote: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: null,
+    },
+  },
+  {
+    sequelize,
+    tableName: "comment",
+    freezeTableName: true,
+    underscored: true,
+    timestamps: false,
+  }
 );
 
 module.exports = Comment;
