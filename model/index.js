4fb9b6941cd5099f786fbeb3ff17ceb026570725
Mustafa Ali <mustafamire@gmail.com>
2021-09-18 09:58:44 +0100

    Change file name from "Recipes" to "Recipe"

--- model/index.js
+++ model/index.js
@@ -1,34 +1,34 @@
 const Comment = require("./comment");
-const Recipe = require("./recipe");
+const Recipe = require("./Recipe");
 const User = require("./user");
 
 User.hasMany(Recipe, {
-    foreignKey: 'user_id'
-  });
-  
-  Recipe.hasMany(Comment, {
-    foreignKey: 'recipe_id'
-  });
-  
-  User.hasMany(Comment, {
-    foreignKey: 'user_id'
-  });  
-  
-  Recipe.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-  Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
-  
-  Comment.belongsTo(Recipe, {
-    foreignKey: 'recipe_id'
-  });
+  foreignKey: 'user_id'
+});
 
-  module.exports = {
-    Comment,
-    Recipe,
-    User,
-  };
-  
+Recipe.hasMany(Comment, {
+  foreignKey: 'recipe_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Recipe.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Recipe, {
+  foreignKey: 'recipe_id'
+});
+
+module.exports = {
+  Comment,
+  Recipe,
+  User,
+};
+
