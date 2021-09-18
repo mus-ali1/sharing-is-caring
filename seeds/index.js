b25824fe811eb4927107707a868a3ba58bca589f
Mustafa Ali <mustafamire@gmail.com>
2021-09-18 11:16:59 +0100

    Create functionality for seeding db with Users

--- seeds/index.js
+++ seeds/index.js
@@ -5,3 +5,13 @@ const userSeeds = require("./user.json");
 const commentSeeds = require("./comment.json");
 const recipeSeeds = require("./recipe.json");
 
+const seedDatabase = async () => {
+    await dbConnection.sync({ force: true });
+
+    console.log("DB Flashed");
+
+    await User.bulkCreate(userSeeds, {
+        individualHooks: true,
+    });
+
+    console.log("Users Added");
