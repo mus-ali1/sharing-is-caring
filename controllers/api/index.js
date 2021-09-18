08eebb11fe033aa9d9bc4a8570f364df18cdc639
Paul Aston <odders17@gmail.com>
2021-09-18 16:00:32 +0100

    Added routes to index.js

---
+++ controllers/api/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const recipeRoutes = require('./RecipeRoutes');
+const commentRoutes = require('./CommentRoutes');
+const userRoutes = require('./UserRoutes');
+
+router.use('/recipe', recipeRoutes);
+router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
