f78a872e6f4de9f307b29187ccd62f166d6070cd
jacqie williamson <jacqieq@me.com>
2021-09-19 15:08:22 +0100

    Create route for an existing user

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -2,7 +2,7 @@ const router = require("express");
 const { User } = require("../../model/User");
 
 const router = router();
-
+//create a new user
 router.post("/", async (req, res) => {
   try {
     const newUser = await User.create(req.body);
@@ -11,3 +11,14 @@ router.post("/", async (req, res) => {
     res.status(400).json(error);
   }
 });
+//logging in an existing user so they already have a user name & cridentials
+router.post("/login", async(req, res) => {
+   try{
+      const user = await User.findOne ({
+         where:{
+            email: req.body.email,
+            
+         }
+      })
+   }
+})
