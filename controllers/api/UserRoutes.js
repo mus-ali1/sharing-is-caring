eacd7a9c1ba943f6d47f47312103aac5afbcabfe
jacqie williamson <jacqieq@me.com>
2021-09-19 16:01:34 +0100

    Added sessions

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -6,41 +6,55 @@ const router = router();
 router.post("/", async (req, res) => {
   try {
     const newUser = await User.create(req.body);
-    //req.session.save
+
+    //adding sessions
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = newUser.id;
+
+      res.status(200).json(newUser);
+    });
   } catch (error) {
     res.status(400).json(error);
   }
 });
 //logging in an existing user so they already have a user name & cridentials
-router.post("/login", async(req, res) => {
-   try{
-      const user = await User.findOne ({
-         where:{
-            email: req.body.email,
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!user) {
+      res
+        .status(400)
+        .json({
+          message:
+            "You have provided an incorrect email address and or password",
+        });
+      return;
+    }
 
-         }
-      });
-      if (!user){
-         res
-         .status (400)
-         .json ({message: "You have provided an incorrect email address and or password"});
-         return;
-      }
-      
-      //error handling for not a valid password
+    //error handling for not a valid password
 
-      const validPassword = await user.checkPassword (req.body.password);
-      
-      if (!validPassword){
-         res
-         .status(400)
-         .json ({message: "you have provided an incorrect email and or password"});
-         return;
+    const validPassword = await user.checkPassword(req.body.password);
 
-         
-      }
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({
+          message: "you have provided an incorrect email and or password",
+        });
+      return;
+    }
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = user.id;
 
-      
-   }
-})
-
+      res.json({ user: user, message: "you are now logged in" });
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
