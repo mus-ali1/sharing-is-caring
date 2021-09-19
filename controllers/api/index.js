f48209b1eeda23ef10b9c2fea3b49d7c96e3be65
jacqie williamson <jacqieq@me.com>
2021-09-19 19:38:42 +0100

    disabled unused routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const recipeRoutes = require('./RecipeRoutes');
-const commentRoutes = require('./CommentRoutes');
-const userRoutes = require('./UserRoutes');
+const router = require("express").Router();
+const recipeRoutes = require("./RecipeRoutes");
+const commentRoutes = require("./CommentRoutes");
+const userRoutes = require("./UserRoutes");
 
-router.use('/recipe', recipeRoutes);
-router.use('/comment', commentRoutes);
-router.use('/user', userRoutes);
+//router.use("/recipe", recipeRoutes);
+//router.use("/comment", commentRoutes);
+router.use("/user", userRoutes);
 
-router.use((req, res) => {
+router.use("*", (req, res) => {
   res.status(404).end();
 });
 
