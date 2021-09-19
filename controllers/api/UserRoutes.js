443d268eb43b0c687af8bf571e8fba2ddd6898d5
jacqie williamson <jacqieq@me.com>
2021-09-19 14:55:01 +0100

    added post req for new user & error catcher

--- controllers/api/UserRoutes.js
+++ controllers/api/UserRoutes.js
@@ -3,4 +3,11 @@ const { User } = require("../../model/User");
 
 const router = router();
 
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    //req.session.save
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
