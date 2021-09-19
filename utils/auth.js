2f1ce7795fc8371fee940c1bc1abd4b22946ae1f
Paul Aston <odders17@gmail.com>
2021-09-19 20:52:33 +0100

    Added and exported auth middleware

---
+++ utils/auth.js
@@ -0,0 +1,9 @@
+const authMiddleware = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+};
+
+module.exports = authMiddleware;
