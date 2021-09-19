ab6ead40d6c6bb42a516b552643c439ee5e77df3
jacqie williamson <jacqieq@me.com>
2021-09-19 19:39:16 +0100

    added sequelize & cookies & sessions

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require("express");
 const path = require("path");
 const handlebars = require("express-handlebars");
 const app = express();
-const sequelizeStore = require("connect-session-sequelize");
+//const sequelizeStore = require("connect-session-sequelize");
 const session = require("express-session");
 const hbs = handlebars.create();
 
 const dbConnection = require("./config/connection");
-
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sessConfig = {
+  secret: "this is the dream team's secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: dbConnection,
+  }),
+};
+app.use(session(sessConfig));
 //const routes = require("./controllers");
 const apiRoutes = require("./controllers/api");
-const app = express();
+//const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(apiRoutes);
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -20,6 +29,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(apiRoutes);
 
 dbConnection.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
