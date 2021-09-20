be9b929a749bd3e0d185806c6a4b4fb4326aa54f
Mustafa Ali <mustafamire@gmail.com>
2021-09-20 11:10:31 +0100

    Created get request for comment

--- controllers/api/CommentRoutes.js
+++ controllers/api/CommentRoutes.js
@@ -1,3 +1,14 @@
 const router = require('express').Router();
 const { Comment } = require('../../model');
 const authMiddleware = require("../../utils/auth");
+
+
+// get all comments
+router.get('/', (req, res) => {
+    Comment.findAll()
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
