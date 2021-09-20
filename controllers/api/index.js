23d3dedabea0e26737d8ccede5c126143e38c7cd
Paul Aston <odders17@gmail.com>
2021-09-20 10:51:18 +0100

    add router.use to index js include authMiddleware

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const recipeRoutes = require("./RecipeRoutes");
 const commentRoutes = require("./CommentRoutes");
 const userRoutes = require("./UserRoutes");
+const authMiddleware = require("../../utils/auth");
 
-//router.use("/recipe", recipeRoutes);
+router.use("/recipe",authMiddleware, recipeRoutes);
 //router.use("/comment", commentRoutes);
 router.use("/user", authMiddleware, userRoutes);
 
