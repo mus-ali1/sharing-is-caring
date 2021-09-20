213ae43504cc493616c572e417d0955f4f10b346
Paul Aston <odders17@gmail.com>
2021-09-20 10:26:12 +0100

    Added delete request for recipe

--- controllers/api/RecipeRoutes.js
+++ controllers/api/RecipeRoutes.js
@@ -15,3 +15,24 @@ router.post('/', authMiddleware, async (req, res) => {
   }
 });
 
+router.delete('/:id', authMiddleware, async (req, res) => {
+    try {
+      const recipeData = await Recipe.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!recipeData) {
+        res.status(404).json({ message: 'No project found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(recipeData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  
